19c5a1b997b1d104f3f15e50157c83ed24635328
Forhad96 <forhadairdrop@gmail.com>
2023-09-14 21:50:27 +0600

    install npm proptypes pakage and created componant folder

---
+++ src/component/courses/Courses.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+const Courses = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+Courses.propTypes={
+    
+}
+export default Courses;
